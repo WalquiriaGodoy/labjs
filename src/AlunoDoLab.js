e68c3dc2d6a4a2710eca08b12629e740af446c2b
Walquiria Godoy <walquiriagodoy@gmail.com>
2021-10-16 11:07:50 -0300

    feat(class): add method to validate email

--- src/AlunoDoLab.js
+++ src/AlunoDoLab.js
@@ -53,4 +53,9 @@ export class AlunoDoLab {
 
         return true;
     }
+
+    validaEmail() {
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(this._email).toLowerCase());
+    }
 }
--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import { AlunoDoLab } from "./AlunoDoLab.js";
 const aluno1 = new AlunoDoLab("Marcos", "Vieira da Silva", "34085185802", "marcosrava@gmail.com");
 
 console.log(aluno1.nomeEhValido());
-console.assert(aluno1.cpfEhValido(),"Deve validar o cpf");
+console.log(aluno1.cpfEhValido());
+console.log(aluno1.validaEmail());
